afa364217622359f557db2afe2fb67ff6c89b72d
DavidPK8 <alejodavicho8@gmail.com>
2023-11-07 16:36:25 -0500

    Funcionalidad a datoUsers

---
+++ datosUsers.js
@@ -0,0 +1,28 @@
+let url = "https://jsonplaceholder.typicode.com/users"
+
+const obtenerUsuarios = async() =>{
+
+    let response = await fetch(url)
+
+    if(!response.ok){
+        throw new Error("Ocurrio un Error")
+    }
+
+    let data = await response.json()
+    pintarUsuarios(data)
+
+}
+
+obtenerUsuarios()
+
+const pintarUsuarios = (data) =>{
+
+    let body = ""
+    
+    for(let i = 0; i < data.length; i++){
+        body += `<tr><td>${data[i].id}</td><td>${data[i].name}</td><td>${data[i].email}</td></tr>`;
+    }
+
+    document.getElementById("data").innerHTML = body
+
+}
